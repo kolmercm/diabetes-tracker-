b6399ff697e0318c933f53fdb8773f6e5d08dbc0
kolmercm <kolmercm@gmail.com>
2024-10-09 15:03:03 -0500

    fix test syntax error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:diabetes_tracker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DiabetesTrackerApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
